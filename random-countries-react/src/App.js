469cad2bb883b616db0daa2fd67e7aa2d7517d51
Mehmet Alperen <me.alperen@gmail.com>
2021-09-08 14:20:51 +0300

    first commit

---
+++ random-countries-react/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CountryCard from './components/CountryCard';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <CountryCard />
+    </div>
+  );
+};
+
+export default App;
---
+++ random-countries-react/src/components/CountryCard.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import countries from './countries';
+
+const CountryCard = () => {
+  const [index, setIndex] = useState(0);
+  const { name, capital, language, currency, flag } = countries[index];
+  const randomCountry = () => {
+    let randomCountry = Math.floor(Math.random() * countries.length);
+    setIndex(randomCountry);
+  };
+  return (
+    <div>
+      <h2 className='title'>Random Countries</h2>
+      <div className='card-container'>
+        <div>
+          <img src={flag} alt='' />
+          <h3>Country : {name}</h3>
+          <h3>Capital : {capital}</h3>
+          <h3>Language : {language.name}</h3>
+          <h3>Currency : {currency.name}</h3>
+        </div>
+      </div>
+      <div>
+        <button onClick={randomCountry}>Select Country</button>
+      </div>
+    </div>
+  );
+};
+
+export default CountryCard;
